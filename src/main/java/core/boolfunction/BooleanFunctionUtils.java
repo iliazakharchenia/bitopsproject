9a722a44c29d97f24f37196671000346600c7af2
Ilia Zakharchenia <iliazakh97@gmail.com>
2024-12-02 18:42:00 +0100

    Update BooleanFunctionUtils.java

--- src/main/java/core/boolfunction/BooleanFunctionUtils.java
+++ src/main/java/core/boolfunction/BooleanFunctionUtils.java
@@ -2,6 +2,15 @@ package core.boolfunction;
 
 import core.bit.Bit;
 
+/**
+ * This utils class in used for
+ * from int value.
+ * <br>
+ * Example: boolean[] x = BitUtils.bitSequenceFromNumber(5);
+ * <br>
+ * // x == [true, false, true] ('101' in 2ns)
+ *
+ */
 public abstract class BooleanFunctionUtils {
 
     public static Bit process(BooleanFunction function, Bit...bits) {
